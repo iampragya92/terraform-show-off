0e21a01a1bc53941ccc87ace33d50abd9a9dd3b1
Pragya Dubey <pragyadubey@Mac.communityfibre.co.uk>
2025-07-21 22:05:47 +0100

    module name modified for forntend and backend subnet

--- dev/main.tf
+++ dev/main.tf
@@ -2,36 +2,36 @@
 
 module "resource_group" {
 
-    source = "../modules/azurerm_resource_group"
-    resource_group_name = "rg-todoapp"
-    location = "uksouth"
+  source              = "../modules/azurerm_resource_group"
+  resource_group_name = "rg-todoapp"
+  location            = "uksouth"
 }
 
 #code to create virtual network for todoapp
 module "vnet" {
-    source = "../modules/azurerm_virtual_network"
-    vnet_name = "vnet-todoapp"
-    resource_group_name = "rg-todoapp"
-    location = "uksouth"
-    address_space = ["10.0.0.0/16"]
-  
+  source              = "../modules/azurerm_virtual_network"
+  vnet_name           = "vnet-todoapp"
+  resource_group_name = "rg-todoapp"
+  location            = "uksouth"
+  address_space       = ["10.0.0.0/16"]
+
 }
 
 # module to create frontend subnet
-module "subnet" {
-    source = "../modules/azurerm_subnet"
-    subnet_name = "frontend-todoapp"
-    resource_group_name = "rg-todoapp"
-    vnet_name = "vnet-todoapp"
-    address_prefixes = ["10.0.1.0/24"]
+module "frontend_subnet" {
+  source              = "../modules/azurerm_subnet"
+  subnet_name         = "frontend-todoapp"
+  resource_group_name = "rg-todoapp"
+  vnet_name           = "vnet-todoapp"
+  address_prefixes    = ["10.0.1.0/24"]
 }
 
 
 # module to create backend subnet
-module "subnet" {
-    source = "../modules/azurerm_subnet"
-    subnet_name = "backend-todoapp"
-    resource_group_name = "rg-todoapp"
-    vnet_name = "vnet-todoapp"
-    address_prefixes = ["10.0.2.0/24"]
+module "backend_subnet" {
+  source              = "../modules/azurerm_subnet"
+  subnet_name         = "backend-todoapp"
+  resource_group_name = "rg-todoapp"
+  vnet_name           = "vnet-todoapp"
+  address_prefixes    = ["10.0.2.0/24"]
 }
--- dev/provider.tf
+++ dev/provider.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     azurerm = {
-        source = "hashicorp/azurerm"
-        version = "4.28.0"
+      source  = "hashicorp/azurerm"
+      version = "4.28.0"
     }
   }
 }
@@ -11,7 +11,7 @@ terraform {
 
 provider "azurerm" {
 
-    features {}
-    subscription_id = ""
-  
+  features {}
+  subscription_id = "1f196f8e-ab8f-4746-a1f5-1798c2228e8f"
+
 }
